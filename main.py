2c95537dd90482c0f9c3b92504e631e0feb063cc
OBKTV JOBK-DTV <65348648+takkunplay@users.noreply.github.com>
2021-12-25 21:27:47 +0000

    v1.01

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-import httplib2, random
-#import imgutil
-from googleapiclient.discovery import build
 import os
 import pprint
 import time
 import urllib.error
 import urllib.request, requests
-import discord
+import discord,random
 from server import keep_alive
-
+from replit import db
 TOKEN = os.environ['token']
 btid = [""]
 # 接続に必要なオブジェクトを生成
@@ -31,13 +28,37 @@ async def on_ready():
   print("logined")
   game = discord.Game("=help")
   await client.change_presence(status=discord.Status.online, activity=game)
-
-
+  channel=client.get_channel(922424600107831327)
 @client.event
 async def on_message(message):
+  if message.content[:6]=="=money":
+    moneys=db["money"]
+    ido=str(message.author.id)
+    re=ido in moneys
+    if re==False:
+      money=0
+    else:
+      money=moneys[ido]
+    await message.reply(message.author.mention+":"+str(money)+"A")
+  if random.randint(0,9)==0:
+    ido=str(message.author.id)
+    moneys=db["money"]
+    re=ido in moneys
+    if re==False:
+      money=0
+    else:
+      money=moneys[ido]
+    db["money"][ido]=money+1
+
+    
+  #ここから先bot立入禁止
   if message.author.bot:
     return
-
-
+  if message.content=="=help":
+    await message.reply("""
+=kaso:過疎を防止します。
+=money:所持金を表示します""")
+  if message.content=="=kaso":
+    await message.channel.send("https://cdn.discordapp.com/attachments/906399117692010576/914472044970795028/kasokin.mov")
 keep_alive()
 client.run(TOKEN)
