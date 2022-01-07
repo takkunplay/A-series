5cea0d6b6ecf1d48af405052497e97deb7585e5a
OBKTV JOBK-DTV <65348648+takkunplay@users.noreply.github.com>
2022-01-07 12:19:23 +0000

    1.2

--- main.py
+++ main.py
@@ -10,19 +10,32 @@ from discord.ext import tasks
 import xml.etree.ElementTree as ET
 TOKEN = os.environ['token']
 btid = [""]
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='=')
 # 接続に必要なオブジェクトを生成
 intents = discord.Intents.all()
 intents.members = True
 client = discord.Client(intents=intents)
+
 # parameters
 STM="=help|あゆさんまじ神"
 #imgutil.mkdir(path)
-mutes=[159985870458322944,894191491277258752,411916947773587456]
+mutes=[533698325203910668,159985870458322944,894191491277258752,411916947773587456,282859044593598464]
+jug=["吐き気を催す　-3点",":-1: -2点","良くはない -1点","普通 0点","悪くはない　1点",":+1: 2点","んふーんっっっｆ 3点"]
 #page_limit = 3 # 検索ページ数
 #startIndex = 1
 response = []
 img_list = []
 #db["kaso"]=311
+@tasks.loop(seconds=1)
+async def looping():
+  try:
+    channel=client.get_channel(906399117692010576)
+    await channel.send(s)
+    os.remove("/home/pi/メッセージ")
+  except:
+    pass
 sex="丗糶背瀬畝競施世攻責勢谷扠設政生性製成説選聖"
 @client.event
 async def on_ready():
@@ -35,23 +48,12 @@ async def on_ready():
   dm=await user.create_dm()
   #await channel.send("=help g")
   channel=client.get_channel(912536602071420969)
-  await channel.connect()
-  guild=client.get_guild(876782808847220786)
   
-  while 1==1:
-    if 1640962789==int(time.time()):
-      break
-    await asyncio.sleep(0.5)
+  await channel.connect()
   channel=client.get_channel(906399117692010576)
-
-  await channel.send("日本年腰10秒前！")
-  await asyncio.sleep(1)
-  guild.voice_client.play(discord.FFmpegPCMAudio("10sec.wav"))
-  await asyncio.sleep(10)
-  await channel.send("https://cdn.discordapp.com/attachments/906399117692010576/926365032919465994/superup.mov")
-  guild.voice_client.play(discord.FFmpegPCMAudio("superup.mp3"))
-  await asyncio.sleep(7)
-  guild.voice_client.play(discord.FFmpegPCMAudio("goodsad.mp3"))
+  if db["rb"]==True:
+    await channel.send(":+1:")
+  db["rb"]=False
 @client.event
 async def on_message(message):
   if message.content[:6]=="=money":
@@ -132,11 +134,13 @@ https://onl.tw/EnNEE4u :ステータスを表示します""")
 =g bh:全財産の半分を倍にします
 =g ba:全財産を倍にします""")
   if message.content=="=kaso":
-    rnd=random.randint(0,5)
+    rnd=random.randint(0,6)
     if rnd==0:
       await message.channel.send("https://cdn.discordapp.com/attachments/906399117692010576/925178035668402186/kasokin.mp4")
     elif rnd==1:
       await message.channel.send("https://cdn.discordapp.com/attachments/906399117692010576/926750544683487242/kasokin.mov")
+    elif rnd==2:
+      await message.channel.send("https://cdn.discordapp.com/attachments/906399117692010576/928447644592902174/kasokin.mov")
     else:
       await message.channel.send("https://cdn.discordapp.com/attachments/906399117692010576/914472044970795028/kasokin.mov")
   if (message.content=="=kaso") or (message.content=="/kaso") or (message.content=="/kaso_n") or (message.content=="/kaso_i"):
@@ -155,14 +159,20 @@ https://onl.tw/EnNEE4u :ステータスを表示します""")
     await message.channel.send("Hey! <@!890178063210545182>!")
   if message.content=="=urs":
     try:
-      message.guild.Voice_Client.stop()
+      message.guild.voice_client.stop()
     except:
       pass
     message.guild.voice_client.play(discord.FFmpegPCMAudio("うるさいわボケー.mp3"))
+  if message.content=="=resa":
+    try:
+      message.guild.voice_client.stop()
+    except:
+      pass
+    message.guild.voice_client.play(discord.FFmpegPCMAudio("レさ.mp3"))
   if message.content=="=ynk":
     me=await message.guild.fetch_member(441098719803211788)
     try:
-      message.guild.Voice_Client.stop()
+      message.guild.voice_client.stop()
     except:
       pass
     #guild=client.get_guild(876782808847220786)
@@ -178,12 +188,49 @@ https://onl.tw/EnNEE4u :ステータスを表示します""")
       if (user.voice==None) or (user.voice.channel.id!=me.voice.channel.id):
         continue
       await user.edit(mute=False)
+  if message.content=="=saens":
+    try:
+      message.guild.voice_client.stop()
+    except:
+      pass
+    message.guild.voice_client.play(discord.FFmpegPCMAudio("SAENSGTOHK.mp3"))
   if message.content=="=10":
     try:
-      message.guild.Voice_Client.stop()
+      message.guild.voice_client.stop()
     except:
       pass
     message.guild.voice_client.play(discord.FFmpegPCMAudio("10sec.wav"))
+  #過疎キンボイス
+  if message.content=="=kasov":
+    try:
+      message.guild.voice_client.stop()
+    except:
+      pass
+    message.guild.voice_client.play(discord.FFmpegPCMAudio("kasokin.mp3"))
+  if message.content=="/rns":
+    if random.randint(0,14)==0:
+      
+      try:
+        message.guild.voice_client.stop()
+        user=await message.guild.fetch_member(894191491277258752)
+        await user.edit(mute=True)
+      except:
+        pass
+      sr=["5696600","7095110"]
+      srn=sr[random.randint(0,len(sr)-1)]
+      await message.reply(srn)
+      message.guild.voice_client.play(discord.FFmpegPCMAudio(srn+".mp3"))
+      if srn=="5696600":
+        await asyncio.sleep(2)
+      if srn=="7095110":
+        await asyncio.sleep(3)
+      try:
+        user=await message.guild.fetch_member(894191491277258752)
+        await user.edit(mute=False)
+      except:
+        pass
+  if message.content=="=move":
+     await message.guild.voice_client.move_to(message.author.voice.channel)
   if message.content=="=ns":
     role = message.guild.get_role(919543222450159636)
     if None==discord.utils.get(message.author.roles, name='にんじんしりしり'):
@@ -196,9 +243,75 @@ https://onl.tw/EnNEE4u :ステータスを表示します""")
       await message.author.add_roles(role)
     else:
       await message.author.remove_roles(role)
+  if message.content=="=ASD":
+    await message.reply(":sunglasses:A-serise Special Delete:sunglasses:\n実行できるか確認しています...")
+    await asyncio.sleep(3)
+    if random.randint(0,1)==0:
+      await message.reply("問題が発生しました。しばらくしてからやり直してください。")
+      return
+    await message.channel.send("準備しています...")
+    await asyncio.sleep(5)
+    await message.channel.send("Discordに破壊の申請をしています...")
+    await asyncio.sleep(1)
+    await message.channel.send("破壊するサーバーは:"+message.guild.name+"です。\n実行する場合は「お前はこれで死ね！」と入力してください")
+    channel = message.channel
+    user=message.author
+    def check(m):
+      # メッセージが `おはよう` かつ メッセージを送信したチャンネルが
+      # コマンドを打ったチャンネルという条件
+      return (m.channel == channel) and (m.author==user)
+    try:
+      # wait_forを用いて、イベントが発火し指定した条件を満たすまで待機する
+      msg = await client.wait_for('message', check=check, timeout=30)
+      # wait_forの1つ目のパラメータは、イベント名の on_がないもの
+      # 2つ目は、待っているものに該当するかを確認する関数 (任意)
+      # 3つ目は、タイムアウトして asyncio.TimeoutError が発生するまでの秒数
+      # asyncio.TimeoutError が発生したらここに飛ぶ
+    except asyncio.TimeoutError:
+      await message.channel.send("取り消しました。")
+      return
+    if msg.content!="お前はこれで死ね！":
+      await message.channel.send("取り消しました。")
+      return
+    await message.channel.send("全チャンネルの内容を取得しています...")
+    await asyncio.sleep(5)
+    await message.channel.send("破壊トークンでアクセスしています...")
+    await asyncio.sleep(10)
+    await message.channel.send("破壊しています...")
+    await asyncio.sleep(15)
+    await message.reply("えー")
+    await asyncio.sleep(2)
+    await message.reply("嘘です")
+    await asyncio.sleep(5)
+    if random.randint(0,2)==0:
+      await message.channel.send("<@!"+str(message.author.id)+">さん、こんなんに騙されるなんて思ってもいませんでした。\n罰としてあなたの所持金をすべて没収します。")
+      db["money"][str(message.author.id)]=0
+  if message.content=="=j":
+    if message.reference==None:
+      await message.delete()
+      await message.channel.send(jug[random.randint(0,len(jug)-1)])
+    else:
+      await message.delete()
+      rm=await message.channel.fetch_message(message.reference.message_id)
+      await rm.reply(jug[random.randint(0,len(jug)-1)])
+  if message.content=="=join":
+    await message.author.voice.channel.connect()
+  if message.content[:5]=="=nick":
+    me=await message.guild.fetch_member(441098719803211788)
+    await me.edit(nick=message.content[6:])
   #bot管理者以外立入禁止
   if (message.author.id!=425948316334030848) and (message.author.id!=441098719803211788):
     return
+  if message.content=="=rb":
+    await message.reply("再起動しています...")
+    import subprocess
+    while True:
+      p = subprocess.Popen("python3 main.py", shell=True).wait()
+      if p != 0:
+        continue
+      else:
+        break
+  
   if message.content[:4]=="=del":
     db["money"][message.content[5:]]=0
     await message.reply("<@!"+message.content[5:]+">の財産\nhttps://cdn.discordapp.com/attachments/906399117692010576/925892604950880256/kieuseta.mov")
@@ -210,6 +323,8 @@ https://onl.tw/EnNEE4u :ステータスを表示します""")
       await message.delete()
       rm=await message.channel.fetch_message(message.reference.message_id)
       await rm.reply(message.content[5:])
-  
-keep_alive()
-client.run(TOKEN)
+rb=keep_alive()
+try:
+  client.run(TOKEN)
+except:
+  pass
--- server.py
+++ server.py
@@ -1,15 +1,31 @@
 from flask import Flask
 from threading import Thread
-
+from replit import db
 app = Flask("")
-
+from markupsafe import escape
 @app.route("/")
 def main():
   return "ヒカキンTVでケツの穴とか言ったことあんまないけど"
-
+@app.route('/money/<user>')
+def show_user_profile(user):
+    # show the user profile for that user
+    return str(db["money"][user])
+@app.route("/login")
+@app.route("/kaso")
+def kaso():
+    with open("kaso.html") as f:
+      s = f.read()
+      return s
 def run():
-  app.run("0.0.0.0", port=8080)
+  try:
+    app.run("0.0.0.0", port=8080)
+  except:
+    db["rb"]=True
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+  try:
+    t = Thread(target=run)
+    t.start()
+    return False
+  except:
+    return True
